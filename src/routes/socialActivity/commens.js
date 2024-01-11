9f7cf54a17193b944181dce1d896442925cfec3c
Waqas Ahmad <wa606544@gmail.com>
2024-01-11 14:47:57 +0500

    Project setup front end and backend

--- src/routes/socialActivity/commens.js
+++
@@ -1,8 +0,0 @@
-import socialCommentController from "../../controller/social-activity/comments.js";
-
-import { Router } from "express";
-const socialCommentRouter =Router()
-
-socialCommentRouter.post("/add",socialCommentController.add)
-
-export default socialCommentRouter
